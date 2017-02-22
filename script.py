6a97e38eb2eeafc9c5195f48939e0c5d4de2c556
Caleb Boylan <calebboylan@gmail.com>
2017-02-22 14:54:14 -0800

    Add --list option

--- script.py
+++ script.py
@@ -5,8 +5,17 @@ import zipcode
 
 args = sys.argv
 
+if "--list" in args:
+    simple_output = True
+    args.remove("--list")
+
+else:
+    simple_output = False
+
 if len(args) <= 1:
     print("Please pass in the starting zipcode as arg 1 and the number of miles around the zipcode you wish to search as arg 2")
+    print("example: python script.py [--list] <zip_code> <miles>")
+    sys.exit(1)
 
 start = zipcode.isequal(args[1])
 
@@ -17,6 +26,9 @@ zipcode_list = []
 for i in zipcodes:
     i_dict = i.to_dict()
     if i_dict['decommisioned'] == "FALSE":
-        zipcode_list.append(i_dict)
+        if simple_output:
+            zipcode_list.append(i_dict['zip'])
+        else:
+            zipcode_list.append(i_dict)
 
 print(json.dumps(zipcode_list))
